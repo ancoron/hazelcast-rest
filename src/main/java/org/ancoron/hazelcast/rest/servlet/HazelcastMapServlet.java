189e0928046c2660458bd5768a5b4b9bb390bbb3
Ancoron Luciferis <ancoron.luciferis@gmail.com>
2016-07-03 23:41:05 +0200

    First options for configuring maps

--- src/main/java/org/ancoron/hazelcast/rest/servlet/HazelcastMapServlet.java
+++ src/main/java/org/ancoron/hazelcast/rest/servlet/HazelcastMapServlet.java
@@ -22,6 +22,8 @@ import java.nio.file.FileAlreadyExistsException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,6 +33,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.stream.JsonWriter;
 import com.hazelcast.config.EvictionPolicy;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.MapConfig;
@@ -48,9 +51,13 @@ import static javax.servlet.http.HttpServletResponse.*;
  * @author ancoron
  */
 public class HazelcastMapServlet extends HttpServlet
-        implements DistributedObjectListener {
+        implements DistributedObjectListener
+{
 
     private static final long serialVersionUID = -7084487112704157155L;
+
+    private static final Logger LOG = Logger.getLogger(HazelcastMapServlet.class
+            .getName());
 
     protected static final int T_APPLICATION_OCTETSTREAM = 0;
     protected static final int T_IMAGE_GIF = 8;
@@ -73,17 +80,20 @@ public class HazelcastMapServlet extends HttpServlet
     protected static final String TYPE_APPLICATION_JSON = "application/json";
 
     private static final Pattern ROUTE_BUCKET = Pattern.compile("^/([^/]+)$");
-    private static final Pattern ROUTE_KEY = Pattern.compile("^/([^/]+)/([^/]+)$");
+    private static final Pattern ROUTE_KEY = Pattern.compile(
+            "^/([^/]+)/([^/]+)$");
 
     protected HazelcastInstance hazelcast;
     protected final ConcurrentMap<String, Long> bucketCreation
             = new ConcurrentHashMap<>();
 
-    public HazelcastInstance getHazelcast() {
+    public HazelcastInstance getHazelcast()
+    {
         return hazelcast;
     }
 
-    public void setHazelcast(HazelcastInstance hazelcast) {
+    public void setHazelcast(HazelcastInstance hazelcast)
+    {
         this.hazelcast = hazelcast;
         if (hazelcast != null) {
             hazelcast.addDistributedObjectListener(this);
@@ -91,34 +101,122 @@ public class HazelcastMapServlet extends HttpServlet
     }
 
     @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
         final String path = req.getPathInfo();
         Matcher matcher = ROUTE_KEY.matcher(path);
 
-        try {
-            if (matcher.matches()) {
-                final String map = matcher.group(1);
-                final String key = matcher.group(2);
-
-                verifyBucket(map);
-
-                deleteValue(map, key);
-            } else if ((matcher = ROUTE_BUCKET.matcher(path)).matches()) {
-                final String map = matcher.group(1);
-                deleteBucket(map);
-                resp.sendError(SC_METHOD_NOT_ALLOWED);
+        if (matcher.matches()) {
+            final String map = matcher.group(1);
+            final String key = matcher.group(2);
+            if (getValue(map, key) == null) {
+                doPut(req, resp);
             } else {
-                resp.sendError(SC_NOT_FOUND);
+                resp.sendError(SC_CONFLICT,
+                        "Key already exists in bucket '" + map + "': " + key);
             }
-        } catch (FileNotFoundException x) {
-            resp.sendError(SC_NOT_FOUND, x.getMessage());
+        } else if ((matcher = ROUTE_BUCKET.matcher(path)).matches()) {
+            final String map = matcher.group(1);
+            String ttl_str = req.getParameter("ttl");
+            String backups_str = req.getParameter("backups");
+            String maxdata_str = req.getParameter("maxData");
+            try {
+                final int ttl;
+                if (ttl_str != null) {
+                    // no negative TTL, please
+                    ttl = Math.max(0, Integer.valueOf(ttl_str));
+                } else {
+                    ttl = 300; // 5 minutes per default
+                }
+
+                final int backups;
+                if (backups_str != null) {
+                    // no negative backups, please
+                    backups = Math.max(0, Integer.valueOf(backups_str));
+                } else {
+                    backups = 0; // no backup by default
+                }
+
+                final int maxdata;
+                if (maxdata_str != null) {
+                    // no negative size, please
+                    maxdata = Math.max(0, Integer.valueOf(maxdata_str));
+                } else {
+                    maxdata = 128; // 128 MiB of data by default
+                }
+
+                createBucket(map, ttl, backups, maxdata);
+
+                resp.setStatus(SC_NO_CONTENT);
+            } catch (FileAlreadyExistsException x) {
+                sendError(resp, SC_CONFLICT, "Bucket already exists: " + map, x);
+            } catch (NumberFormatException x) {
+                sendError(resp, SC_BAD_REQUEST, "Invalid parameter value specified", x);
+            }
+        } else {
+            sendError(resp, SC_NOT_FOUND, "Resource not found: " + path);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+        final String path = req.getPathInfo();
+        Matcher matcher = ROUTE_KEY.matcher(path);
+
+        if (matcher.matches()) {
+            String map = matcher.group(1);
+            String key = matcher.group(2);
+
+            try {
+                byte[] bytes = getValue(map, key);
+                if (bytes == null) {
+                    sendError(resp, SC_NOT_FOUND,
+                            "Key does not exist in bucket '" + map + "': " + key);
+                } else if (bytes.length == 1) {
+                    resp.setContentType(mapContentType(bytes[0]));
+                    resp.setContentLength(0);
+                    resp.setStatus(SC_NO_CONTENT);
+                } else {
+                    resp.setStatus(SC_OK);
+                    resp.setContentType(mapContentType(bytes[0]));
+                    resp.setContentLength(bytes.length - 1);
+                    try (final ServletOutputStream out = resp.getOutputStream()) {
+                        out.write(bytes, 1, bytes.length - 1);
+                    }
+                }
+            } catch (FileNotFoundException x) {
+                sendError(resp, SC_NOT_FOUND, "Bucket not found: " + map, x);
+            }
+        } else if ((matcher = ROUTE_BUCKET.matcher(path)).matches()) {
+            String map = matcher.group(1);
+            try {
+                verifyBucket(map);
+                IMap<String, byte[]> m = hazelcast.getMap(map);
+                MapConfig config = hazelcast.getConfig().getMapConfig(map);
+                resp.setStatus(SC_OK);
+                try (JsonWriter writer = new JsonWriter(resp.getWriter())) {
+                    writer.beginObject();
+                    writer.name("count").value(m.size());
+                    writer.name("ttl").value(config.getTimeToLiveSeconds());
+                    writer.name("backups").value(config.getBackupCount());
+                    writer.name("maxData").value(config.getMaxSizeConfig().getSize());
+                    writer.endObject();
+                }
+            } catch (FileNotFoundException x) {
+                sendError(resp, SC_NOT_FOUND, "Bucket not found: " + map, x);
+            }
+        } else {
+            sendError(resp, SC_NOT_FOUND, "Resource not found: " + path);
         }
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
+            throws ServletException, IOException
+    {
         final String path = req.getPathInfo();
         Matcher matcher = ROUTE_KEY.matcher(path);
 
@@ -147,7 +245,8 @@ public class HazelcastMapServlet extends HttpServlet
             } catch (FileNotFoundException x) {
                 resp.sendError(SC_NOT_FOUND, x.getMessage());
             } catch (IOException x) {
-                log("Unable to set value for bucket '" + map + "' and key '" + key + "'", x);
+                log("Unable to set value for bucket '" + map + "' and key '" + key + "'",
+                        x);
                 resp.sendError(SC_INTERNAL_SERVER_ERROR,
                         x.getMessage());
             }
@@ -160,30 +259,33 @@ public class HazelcastMapServlet extends HttpServlet
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
     {
         final String path = req.getPathInfo();
         Matcher matcher = ROUTE_KEY.matcher(path);
 
-        if (matcher.matches()) {
-            final String map = matcher.group(1);
-            final String key = matcher.group(2);
-            if (getValue(map, key) == null) {
-                doPut(req, resp);
+        try {
+            if (matcher.matches()) {
+                final String map = matcher.group(1);
+                final String key = matcher.group(2);
+
+                verifyBucket(map);
+
+                deleteValue(map, key);
+            } else if ((matcher = ROUTE_BUCKET.matcher(path)).matches()) {
+                final String map = matcher.group(1);
+                deleteBucket(map);
+                resp.sendError(SC_METHOD_NOT_ALLOWED);
             } else {
-                resp.sendError(SC_CONFLICT,
-                        "Key already exists in bucket '" + map + "': " + key);
+                sendError(resp, SC_NOT_FOUND, "Resource not found: " + path);
             }
-        } else if ((matcher = ROUTE_BUCKET.matcher(path)).matches()) {
-            final String map = matcher.group(1);
-            createBucket(map, 60, 0, 128);
-        } else {
-            resp.sendError(SC_NOT_FOUND);
+        } catch (FileNotFoundException x) {
+            sendError(resp, SC_NOT_FOUND, x.getMessage());
         }
     }
 
-    public void createBucket(String map, int ttl, int redundancy, int mib)
+    public void createBucket(String map, int ttl, int backups, int mib)
             throws IOException
     {
         if (bucketCreation.containsKey(map)) {
@@ -191,12 +293,13 @@ public class HazelcastMapServlet extends HttpServlet
                     "Bucket already exists: " + map);
         }
 
-        Map<String, MapConfig> mapConfigs = hazelcast.getConfig().getMapConfigs();
+        Map<String, MapConfig> mapConfigs = hazelcast.getConfig()
+                .getMapConfigs();
         MapConfig config = new MapConfig(map);
         config.setTimeToLiveSeconds(ttl);
         config.setEvictionPolicy(EvictionPolicy.LRU);
         config.setInMemoryFormat(InMemoryFormat.BINARY);
-        config.setBackupCount(redundancy);
+        config.setBackupCount(backups);
 
         int nodes = hazelcast.getCluster().getMembers().size();
         MaxSizeConfig max = new MaxSizeConfig(mib / nodes,
@@ -211,7 +314,8 @@ public class HazelcastMapServlet extends HttpServlet
         hazelcast.getMap(map);
     }
 
-    public void deleteBucket(String map) throws IOException {
+    public void deleteBucket(String map) throws IOException
+    {
         verifyBucket(map);
 
         hazelcast.getConfig().getMapConfigs().remove(map);
@@ -226,7 +330,8 @@ public class HazelcastMapServlet extends HttpServlet
         if (getValue(map, key) != null) {
             hazelcast.getMap(map).delete(key);
         } else {
-            throw new FileNotFoundException("Key not found in bucket '" + map + "': " + key);
+            throw new FileNotFoundException(
+                    "Key not found in bucket '" + map + "': " + key);
         }
     }
 
@@ -251,57 +356,48 @@ public class HazelcastMapServlet extends HttpServlet
         m.set(key, data);
     }
 
-    protected void verifyBucket(String map) throws FileNotFoundException {
+    protected void verifyBucket(String map) throws FileNotFoundException
+    {
         if (!bucketCreation.containsKey(map)) {
             throw new FileNotFoundException("Bucket not found: " + map);
         }
     }
 
-    public byte[] getValue(String map, String key) throws IOException {
+    public byte[] getValue(String map, String key) throws IOException
+    {
         verifyBucket(map);
 
         IMap<String, byte[]> m = hazelcast.getMap(map);
         return m.get(key);
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        final String path = req.getPathInfo();
-        Matcher matcher = ROUTE_KEY.matcher(path);
+    public void sendError(HttpServletResponse resp, int status, String message)
+            throws IOException
+    {
+        sendError(resp, status, message, null);
+    }
 
-        if (matcher.matches()) {
-            String map = matcher.group(1);
-            String key = matcher.group(2);
-
-            try {
-                byte[] bytes = getValue(map, key);
-                if (bytes == null) {
-                    resp.sendError(SC_NOT_FOUND);
-                } else if (bytes.length == 1) {
-                    resp.setContentType(mapContentType(bytes[0]));
-                    resp.setContentLength(0);
-                    resp.setStatus(SC_NO_CONTENT);
-                } else {
-                    resp.setStatus(SC_OK);
-                    resp.setContentType(mapContentType(bytes[0]));
-                    resp.setContentLength(bytes.length - 1);
-                    try (final ServletOutputStream out = resp.getOutputStream()) {
-                        out.write(bytes, 1, bytes.length - 1);
-                    }
-                }
-            } catch (FileNotFoundException x) {
-                resp.sendError(SC_NOT_FOUND, "Bucket not found: " + map);
-            }
-        } else if ((matcher = ROUTE_BUCKET.matcher(path)).matches()) {
-            String map = matcher.group(1);
-            resp.setStatus(SC_NO_CONTENT);
+    public void sendError(HttpServletResponse resp, int status, String message,
+            Throwable error) throws IOException
+    {
+        if (error != null) {
+            LOG.log(Level.WARNING, message, error);
         } else {
-            resp.sendError(SC_NOT_FOUND);
+            LOG.log(Level.WARNING, message);
+        }
+
+        resp.setStatus(status);
+        resp.setContentType(TYPE_APPLICATION_JSON);
+
+        try (JsonWriter writer = new JsonWriter(resp.getWriter())) {
+            writer.beginObject();
+            writer.name("error").value(message);
+            writer.endObject();
         }
     }
 
-    protected byte mapContentType(final String contentType) {
+    protected byte mapContentType(final String contentType)
+    {
         if (contentType == null) {
             throw new IllegalArgumentException("No Content-Type specified");
         }
@@ -329,7 +425,8 @@ public class HazelcastMapServlet extends HttpServlet
                 "Content-Type not supported: " + contentType);
     }
 
-    protected String mapContentType(final byte contentType) {
+    protected String mapContentType(final byte contentType)
+    {
         switch (contentType) {
             case T_APPLICATION_JSON:
                 return TYPE_APPLICATION_JSON;
@@ -355,7 +452,8 @@ public class HazelcastMapServlet extends HttpServlet
     }
 
     @Override
-    public void distributedObjectCreated(DistributedObjectEvent event) {
+    public void distributedObjectCreated(DistributedObjectEvent event)
+    {
         if (DistributedObjectEvent.EventType.CREATED == event.getEventType()) {
             String serviceName = event.getServiceName();
             if (MapService.SERVICE_NAME.equals(serviceName)) {
@@ -368,7 +466,8 @@ public class HazelcastMapServlet extends HttpServlet
     }
 
     @Override
-    public void distributedObjectDestroyed(DistributedObjectEvent event) {
+    public void distributedObjectDestroyed(DistributedObjectEvent event)
+    {
         if (DistributedObjectEvent.EventType.DESTROYED == event.getEventType()) {
             String serviceName = event.getServiceName();
             if (MapService.SERVICE_NAME.equals(serviceName)) {
